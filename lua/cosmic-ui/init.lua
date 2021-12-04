5ca4040bcbd424c8b6e40081d8cefc83cff811dc
Matt Leong <mattleong91@gmail.com>
2021-12-03 22:51:37 -0800

    add highlight method

--- lua/cosmic-ui/init.lua
+++ lua/cosmic-ui/init.lua
@@ -4,4 +4,14 @@ M.rename = function(popup_opts, opts)
   return require('cosmic-ui.rename')(popup_opts, opts)
 end
 
+M.highlight = function(group, bg, fg, gui)
+  if gui ~= nil and gui ~= '' then
+    vim.api.nvim_command(('hi %s guibg=%s guifg=%s gui=%s'):format(group, bg, fg, gui))
+  elseif bg == nil then
+    vim.api.nvim_command(('hi %s guifg=%s'):format(group, fg))
+  else
+    vim.api.nvim_command(('hi %s guibg=%s guifg=%s'):format(group, bg, fg))
+  end
+end
+
 return M
